1fdc843a3b2d346fe25d87526fb544abd66cf6c1
mitander <carlmitander@gmail.com>
2023-09-19 17:49:49 +0200

    Handle memory leaks on failure

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,17 @@
 const std = @import("std");
 const peer = @import("peer.zig");
-
-const debug = std.log.debug;
-const info = std.log.info;
-
 const TorrentFile = @import("torrent_file.zig").TorrentFile;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const ally = gpa.allocator();
+    var ally = gpa.allocator();
     defer _ = gpa.deinit();
 
     const data = try std.fs.cwd().readFileAlloc(ally, "./assets/example.torrent", 60_000);
     defer ally.free(data);
 
-    var torrent = try TorrentFile.parse(data, ally);
+    var torrent = try TorrentFile.init(data, ally);
+    std.log.debug("{any}", .{torrent});
     defer torrent.deinit();
 
     const peers = try peer.requestPeers(torrent.announce_urls.items, torrent.info_hash, torrent.total_len, ally);
--- src/torrent_file.zig
+++ src/torrent_file.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const zencode = @import("zencode");
+const log = std.log.scoped(.torrent_file);
 
 const File = struct {
     path: []const u8,
@@ -18,38 +19,45 @@ pub const TorrentFile = struct {
 
     const Self = @This();
 
-    pub fn parse(data: []const u8, ally: std.mem.Allocator) !TorrentFile {
-        const v = try zencode.parse(data, ally);
+    pub fn init(bencode: []const u8, ally: std.mem.Allocator) !TorrentFile {
+        const v = try zencode.parse(bencode, ally);
         defer v.deinit();
         zencode.MapLookupError = error.InvalidBencode;
 
         var announce_urls = std.ArrayList([]const u8).init(ally);
         errdefer announce_urls.deinit();
-        var announce = try zencode.mapLookup(v.root.Map, "announce", .String);
-        if (validUrl(announce)) try announce_urls.append(try ally.dupe(u8, announce));
+
+        errdefer for (announce_urls.items) |val| ally.free(val);
+        if (zencode.mapLookupOptional(v.root.Map, "announce", .String)) |announce| {
+            const owned_announce = try ally.dupe(u8, announce);
+            try announce_urls.append(owned_announce);
+        }
         if (zencode.mapLookupOptional(v.root.Map, "announce-list", .List)) |url_list| {
-            for (url_list.items) |item| if (validUrl(item.String)) {
-                try announce_urls.append(try ally.dupe(u8, item.String));
-            };
+            for (url_list.items) |announce| {
+                const owned_announce = try ally.dupe(u8, announce.String);
+                try announce_urls.append(owned_announce);
+            }
         }
 
         const info = try zencode.mapLookup(v.root.Map, "info", .Map);
+        const name = try zencode.mapLookup(info, "name", .String);
         const pieces = try zencode.mapLookup(info, "pieces", .String);
-        const path = try zencode.mapLookup(info, "name", .String);
         const piece_len: usize = @intCast(try zencode.mapLookup(info, "piece length", .Integer));
         var total_len: usize = @intCast(try zencode.mapLookup(info, "length", .Integer));
 
         var files = std.ArrayList(File).init(ally);
         errdefer files.deinit();
-        try files.append(.{ .path = try ally.dupe(u8, path), .length = total_len });
-        if (zencode.mapLookupOptional(info, "files", .List)) |list| {
-            for (list.items) |file| {
-                const l: usize = @intCast(try zencode.mapLookup(file.Map, "length", .Integer));
-                const p = try zencode.mapLookup(file.Map, "path", .String);
-                total_len += l;
-                try files.append(.{ .path = try ally.dupe(u8, p), .length = l });
-            }
-        }
+        const owned_name = try ally.dupe(u8, name);
+        errdefer ally.free(owned_name);
+        try files.append(.{ .path = owned_name, .length = total_len });
+
+        if (zencode.mapLookupOptional(info, "files", .List)) |list| for (list.items) |file| {
+            const length: usize = @intCast(try zencode.mapLookup(file.Map, "length", .Integer));
+            const owned_path = try ally.dupe(u8, try zencode.mapLookup(file.Map, "path", .String));
+            errdefer ally.free(owned_path);
+            try files.append(.{ .path = owned_name, .length = length });
+            total_len += length;
+        };
 
         const hash_len = 20;
         const num_hashes = pieces.len / hash_len;
@@ -62,17 +70,20 @@ pub const TorrentFile = struct {
             std.mem.copy(u8, &piece_hashes[i], pieces[begin..end]);
         }
 
-        var list = std.ArrayList(u8).init(ally);
-        defer list.deinit();
+        var info_bencoded = std.ArrayList(u8).init(ally);
+        defer info_bencoded.deinit();
         const value_info = zencode.Value{ .Map = info };
-        try value_info.encode(list.writer());
+        try value_info.encode(info_bencoded.writer());
         var info_hash: [20]u8 = undefined;
-        std.crypto.hash.Sha1.hash(list.items, info_hash[0..], std.crypto.hash.Sha1.Options{});
+        std.crypto.hash.Sha1.hash(info_bencoded.items, info_hash[0..], std.crypto.hash.Sha1.Options{});
+
+        const owned_pieces = try ally.dupe(u8, pieces);
+        errdefer ally.free(owned_pieces);
 
         return TorrentFile{
             .announce_urls = announce_urls,
             .info_hash = info_hash,
-            .pieces = try ally.dupe(u8, pieces),
+            .pieces = owned_pieces,
             .piece_hashes = piece_hashes,
             .piece_len = piece_len,
             .total_len = total_len,
@@ -91,46 +102,31 @@ pub const TorrentFile = struct {
     }
 };
 
-fn validUrl(url: []const u8) bool {
-    if (url.len < 7) return false;
-    return (std.mem.eql(u8, url[0..7], "http://") or std.mem.eql(u8, url[0..8], "https://"));
-}
-
 const testing = std.testing;
-const expectEqual = testing.expectEqual;
-const expectEqualStrings = testing.expectEqualStrings;
 
-test "parse torrent file" {
-    const data = try std.fs.cwd().readFileAlloc(testing.allocator, "./deps/zencode/assets/example.torrent", 100_000);
+test "create torrent file" {
+    const data = try std.fs.cwd().readFileAlloc(testing.allocator, "./assets/example.torrent", 100_000);
     defer testing.allocator.free(data);
-    var torrent = try TorrentFile.parse(data, testing.allocator);
+    var torrent = try TorrentFile.init(data, testing.allocator);
     defer torrent.deinit();
 
     const hash_len = 20;
     const piece_len = torrent.pieces.len / hash_len;
 
-    try expectEqual(hash_len, torrent.info_hash.len);
-    try expectEqual(piece_len, torrent.piece_hashes.len);
-    try expectEqual(@as(usize, 262_144), torrent.piece_len);
-    try expectEqual(@as(usize, 50_000), torrent.pieces.len);
-    try expectEqual(@as(usize, 1), torrent.files.items.len);
-    try expectEqualStrings("debian-mac-12.1.0-amd64-netinst.iso", torrent.files.items[0].path);
-    try expectEqualStrings("http://bttracker.debian.org:6969/announce", torrent.announce_urls.items[0]);
+    try testing.expectEqual(hash_len, torrent.info_hash.len);
+    try testing.expectEqual(piece_len, torrent.piece_hashes.len);
+    try testing.expectEqual(@as(usize, 262_144), torrent.piece_len);
+    try testing.expectEqual(@as(usize, 50_000), torrent.pieces.len);
+    try testing.expectEqual(@as(usize, 1), torrent.files.items.len);
+    try testing.expectEqualStrings("debian-mac-12.1.0-amd64-netinst.iso", torrent.files.items[0].path);
+    try testing.expectEqualStrings("http://bttracker.debian.org:6969/announce", torrent.announce_urls.items[0]);
 }
 
-test "parse torrent multiple files" {
-    var torrent = try TorrentFile.parse("d8:announce14:http://foo.com4:infod6:lengthi20e12:piece lengthi20e6:pieces20:0123456789012345678904:name11:example.iso5:filesld6:lengthi40e4:path8:test.txteeee", testing.allocator);
+test "create torrent with multiple files" {
+    var torrent = try TorrentFile.init("d8:announce14:http://foo.com4:infod6:lengthi20e12:piece lengthi20e6:pieces20:0123456789012345678904:name11:example.iso5:filesld6:lengthi40e4:path8:test.txteeee", testing.allocator);
     defer torrent.deinit();
-    try expectEqual(@as(usize, 20), torrent.files.items[0].length);
-    try expectEqualStrings("example.iso", torrent.files.items[0].path);
-    try expectEqual(@as(usize, 40), torrent.files.items[1].length);
-    try expectEqualStrings("test.txt", torrent.files.items[1].path);
-}
-
-test "parse url" {
-    try testing.expect(validUrl("http://www.exampleurl.com") == true);
-    try testing.expect(validUrl("https://www.exampleurl.com") == true);
-    try testing.expect(validUrl("http//www.exampleurl.com") == false);
-    try testing.expect(validUrl("htt://www.exampleurl.com") == false);
-    try testing.expect(validUrl("www.exampleurl.com") == false);
+    try testing.expectEqual(@as(usize, 20), torrent.files.items[0].length);
+    try testing.expectEqualStrings("example.iso", torrent.files.items[0].path);
+    try testing.expectEqual(@as(usize, 40), torrent.files.items[1].length);
+    try testing.expectEqualStrings("test.txt", torrent.files.items[1].path);
 }
